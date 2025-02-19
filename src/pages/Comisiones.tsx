15733e4768c9eb73653234cceada4c6f5db7cbb5
Wladi <figuerawladimir@gmail.com>
2025-02-19 11:12:02 -0400

    Deploy v0.1 componets

--- src/pages/Comisiones.tsx
+++ src/pages/Comisiones.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Background } from "../components/Background";
 import { MobileNav } from "../components/MobileNav";
-import { NivelAlcanzadoComisiones } from "../components/NivelAlcanzadoComisiones";
+import { NivelAlcanzadoComisiones } from "../components/NIvelAlcanzadoComisiones";
+
 
 export const Comisiones = () => {
   const [username, setUsername] = useState("");
