a60d88db9a77f157a16c44d0d2a8b5ac97d7cd41
Wladimir Figuera <70117039+wladi27@users.noreply.github.com>
2026-02-02 15:55:20 -0400

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,82 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
+import { Dashboard } from './pages/Dashboard';
+import { Perfil } from './pages/Perfil';
+import { Red } from './pages/Red';
+import { Comisiones } from './pages/Comisiones';
+import { Terminos } from './pages/Terminos';
+import { LoginAdmin } from './pages/LoginAdmin';
+import { Admin } from './pages/Admin';
+import { RedAdmin } from './pages/RedAdmin';
+import { PerfilAdmin } from './pages/PerfilAdmin';
+import { CreatePublication } from './pages/Post';
+import { Validar } from './pages/Validar';
+import { TotalUsuarios } from './pages/TotalUsuarios';
+import { TotalReferralCodes } from './pages/TotalCodigos';
+import { TotalWithdrawals } from './pages/TotalRetiros';
+import { TotalApprovedContributions } from './pages/TotalAportes';
+import { CrudComisiones } from './pages/CrudComisiones';
+import WalletApp from './pages/Billetera';
+import { CambiarContrasena } from './pages/Rpass';
+import { RecargarBilletera } from './pages/RecargaAdmin';
+import { DescargarDatosPage } from './pages/DescargarDataPage';
+import { RecargaMasiva } from './pages/RecargaMasiva'; 
+import ReferidosDirectos from './pages/ReferidosDirectos';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { initSocket, disconnectSocket } from './socket';
+import { useAuthStore } from './store/authStore';
+import { BilleterasFaltantes } from './pages/BilleterasFaltantes';
+{/* import ServiceSuspendedNotice from './pages/Suspen'; */}
 
 function App() {
+  const token = useAuthStore((state) => state.token);
+
+  useEffect(() => {
+    if (token) {
+      initSocket(token);
+    }
+
+    return () => {
+      disconnectSocket();
+    };
+  }, [token]);
+
   return (
     <Router>
       <Routes>
+        {/* <Route path="/" element={<ServiceSuspendedNotice />} /> */}
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<Home />} /> {/* Esta línea redirige todas las rutas a Home */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/terminos-y-condiciones" element={<Terminos/>} />
+        <Route path="/BV/auth/login" element={<LoginAdmin />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/billetera" element={<WalletApp />} />
+          <Route path="/perfil" element={<Perfil/>} />
+          <Route path="/red" element={<Red/>} />
+          <Route path="/comisiones" element={<Comisiones/>} />
+          <Route path="/BV/dashboard" element={<Admin />} />
+          <Route path="/BV/red" element={<RedAdmin />} />
+          <Route path="/BV/perfil" element={<PerfilAdmin />} />
+          <Route path="/BV/post" element={<CreatePublication />} />
+          <Route path="/BV/validar" element={<Validar />} />
+          <Route path="/BV/usuarios" element={<TotalUsuarios />} />
+          <Route path="/BV/codes" element={ <TotalReferralCodes />} />
+          <Route path="/BV/retiros" element={ <TotalWithdrawals />} />
+          <Route path="/BV/comisiones" element={<CrudComisiones />} />
+          <Route path="/BV/recarga" element={<RecargarBilletera />} />
+          <Route path="/BV/recarga-masiva" element={<RecargaMasiva />} />
+          <Route path="/BV/aportes" element={<TotalApprovedContributions />} />
+          <Route path="/BV/descargar-datos" element={<DescargarDatosPage />} />
+          <Route path="/BV/restaurar-password" element={<CambiarContrasena />} /> 
+          <Route path="/referidos-directos" element={<ReferidosDirectos />} />
+          <Route path="/admin/billeteras-faltantes" element={<BilleterasFaltantes />} />
+        </Route>
       </Routes>
     </Router>
   );
